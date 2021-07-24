5b3eb1cc7e19ddc95f9b3616de37f62ee82fb704
XavierNiu <a@nxw.name>
2021-07-24 18:09:47 +0800

    triple go-java example

---
+++ example/dubbo/java-client/src/main/java/org/apache/dubbo/Consumer.java
@@ -0,0 +1,9 @@
+package org.apache.dubbo;
+
+public class Consumer {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ example/dubbo/java-client/src/main/java/org/apache/dubbo/User.java
@@ -0,0 +1,4 @@
+package org.apache.dubbo;
+
+public class User {
+}
