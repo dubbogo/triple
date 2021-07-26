676e2941b55e7a32d8454c64a56b00d2e7f76745
XavierNiu <a@nxw.name>
2021-07-26 17:33:52 +0800

    fix waiting_launch.sh bug (#10)

--- example/waiting_launch.sh
+++ example/waiting_launch.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ -z "$1" ]; then
   echo "Provide log file please, like : ./waiting_launch.sh log.sh"
   exit
@@ -7,7 +9,8 @@ PREV_MD5=""
 MD5=""
 
 # wait 150s at most
-for ((i=1; i<=15; i++)); do
+for ((i=1; i<=15; i++));
+do
   sleep 10s
   MD5=$(md5sum $1 | cut -d ' ' -f1)
   if [ "$PREV_MD5" = "$MD5" ]; then
