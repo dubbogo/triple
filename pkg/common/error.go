339553a898ab12ebcde105b04e0a8b3a3eecac62
LaurenceLiZhixin <382673304@qq.com>
2022-03-16 15:35:39 +0800

    Fix: error

--- pkg/common/error.go
+++ pkg/common/error.go
@@ -1,5 +1,13 @@
 package common
 
+import (
+	"github.com/dubbogo/grpc-go/codes"
+	"github.com/dubbogo/grpc-go/status"
+)
+
 type TripleError interface {
 	Stacks() string
+	Code() codes.Code
+	Message() string
+	GRPCStatus() *status.Status
 }
